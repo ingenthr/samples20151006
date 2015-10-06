3867166af8f089cc1d0b236beeac2e0e3e887a6e
Matt Ingenthron <ingenthr@cep.net>
2015-10-06 01:15:46 -0700

    Updated node.js examples.

--- nodejsexamples.js
+++ nodejsexamples.js
@@ -1,17 +1,19 @@
 // Get Document
-bucket.get(key, function (err, resRead) {});
+bucket.get("walter", function (err, resRead) {/* handle results */});
 
 // Insert
-var key = "nodeDevguideExampleRetrieve";
-bucket.insert(key, {test:"Some Test Value"},function(err, res) {});
+var key = "walter";
+bucket.insert(key, {firstname: "Walter", lastname: "White"},function(err, res) {/* handle results */});
 
 // Upsert
+var key = "walter";
+bucket.upsert(key, {firstname: "Walter", lastname: "White"},function(err, res) {/* handle results */});
 
 // Replace
-
+var key = "walter";
+bucket.replace(key, {firstname: "Walter", lastname: "White"},function(err, res) {/* handle results */});
 
 // Query Document
 var N1qlQuery = couchbase.N1qlQuery;
-var query = N1qlQuery.fromString("SELECT airportname, city, country FROM `travel-sample` " +
-"WHERE type='airport' AND city='Reno' ");
-bucket.query(query,function(err,result){});
+var query = N1qlQuery.fromString("SELECT name FROM `travel-sample` AS Airline WHERE id = 5209");
+bucket.query(query,function(err,result){/* handle results */});
